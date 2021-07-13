e168b7a1065eb893ab73e21b37d711cad1af3a70
Anne Wong <directtoanne@gmail.com>
2021-07-13 21:03:09 +0100

    finally working! bless da lawd

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do
+  Task.create(
+    title: Faker::Name.name,
+    details: Faker::String.random,
+    completed: Faker::Boolean.boolean
+  )
+end
